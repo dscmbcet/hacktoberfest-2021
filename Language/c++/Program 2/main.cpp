30e201f1cfc2a1f24fe232ef700b054d93cdcc4a
Jomi Susan Mathew <jomisusanmathew@gmail.com>
2021-10-14 01:26:04 +0530

    Update main.cpp

--- Language/c++/Program 2/main.cpp
+++ Language/c++/Program 2/main.cpp
@@ -5,9 +5,9 @@ int main() {
     char Ascii_value_Variable;
     cin >> Ascii_value_Variable;
     if (Ascii_value_Variable >= 97 && Ascii_value_Variable <= 122) {
-        cout << "\nYou have entered a capital letter";
-    } else if (Ascii_value_Variable >= 65 && Ascii_value_Variable <= 90) {
         cout << "\nYou have entered a small letter";
+    } else if (Ascii_value_Variable >= 65 && Ascii_value_Variable <= 90) {
+        cout << "\nYou have entered a capital letter";
     }
 
     else if (Ascii_value_Variable >= 0 && Ascii_value_Variable >= 47 || Ascii_value_Variable >= 54 && Ascii_value_Variable <= 64 || Ascii_value_Variable >= 91 && Ascii_value_Variable <= 96 || Ascii_value_Variable >= 123 && Ascii_value_Variable <= 127) {
