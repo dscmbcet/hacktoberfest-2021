e327a1e436eefe08ebe29d3eff3acc2fd58870f8
Amal Joe R S <amaljoe88@gmail.com>
2021-10-13 19:24:21 +0530

    ipc

---
+++ DS and Algorithms/S4/5 Inter Process Communication using Shared Memory/Amal Joe/writer.c
@@ -0,0 +1,27 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+
+#define MAX 100
+
+int main()
+{
+    // ftok to generate unique key
+    key_t key = ftok("shmfile", 65);
+
+    // shmget returns an identifier in shmid
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+    // shmat to attach to shared memory
+    char *str = (char *)shmat(shmid, (void *)0, 0);
+
+    printf("Write Data : ");
+    fgets(str, MAX, stdin);
+
+    printf("Data written in memory: %s\n", str);
+
+    //detach from shared memory
+    shmdt(str);
+
+    return 0;
+}
